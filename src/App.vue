d475c7f83a92b0754e6eba50e7d8b63b6364c2fb
Yi-ShuanLi <beauty1997724@gmail.com>
2024-05-14 09:53:02 +0800

    創立檔案

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <div class="header">
+    <Header />
+  </div>
+  <div class="content">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.header {
+  width: 100%;
+  height: 20vh;
+}
+.content{
+  width:100%;
+  height:80vh;
+  text-align:center;
+  display:flex;
+  justify-content:start;
+  align-items:center;
+  flex-direction:column;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <main>
+   <h1>歡迎來到盤子網路購物中心</h1>
+  </main>
+</template>
